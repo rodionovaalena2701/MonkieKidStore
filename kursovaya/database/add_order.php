dfd8c4edc63393670fccb1c48ac7c94e528f2252
Rodionova3course <123074176+rodionovaalena2701@users.noreply.github.com>
2024-05-14 09:14:38 +0300

    Update add_order.php

--- kursovaya/database/add_order.php
+++ kursovaya/database/add_order.php
@@ -6,20 +6,18 @@ if (isset($_POST['number'])) {
     unset($number);
   }
 }
-
 if (empty($number)) {
   exit("Вы ввели не всю информацию, вернитесь назад и заполните все поля!");
 }
 include ('dbconnect.php');
 include ('id.php');
-$result = $mysqli->query("SELECT * FROM orders, products, `status`, users WHERE orders.id_product = products.id_product AND orders.id_client  = users.id_user AND orders.id_status_order = status.id_status");
-while ($myrow = $result->fetch_assoc()) {
-  if ($myrow['login'] == $log) {
-    $user = $myrow['id_user'];
-  }
-  if ($myrow['id_product'] == $id) {
-    $sum = $number * $myrow['cost'];
-  }
+$result = $mysqli->query("SELECT * FROM products, users WHERE id_product = '$id' AND login = '$log'");
+$myrow = $result->fetch_assoc();
+if ($myrow['login'] == $log) {
+  $user = $myrow['id_user'];
+}
+if ($myrow['id_product'] == $id) {
+  $sum = $number * $myrow['cost'];
 }
 if ($myrow['adress'] != NULL) {
   $order = $mysqli->query("INSERT INTO orders (id_client, id_product, id_status_order, quantity_product, order_cost) VALUES ('$user', '$id', 1, '$number', '$sum')");
